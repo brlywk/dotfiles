a8279fc2067cf8c1da15acd650e484071d0f8716
Christoph Suessmeier <mail@suessmeier.net>
2024-01-14 14:18:20 +0100

    Noice.nvim: Suppress "no information" popup and add @recording back to lualine

--- nvim/lua/brlywk/plugins/noice.lua
+++ nvim/lua/brlywk/plugins/noice.lua
@@ -7,8 +7,22 @@ return {
 		"rcarriga/nvim-notify",
 	},
 	config = function()
+		-- to avoid some pesky error notifications
 		require("notify").setup({
 			background_colour = "#000000",
+		})
+
+		-- overwrite lualine to include macro recording message again (@recording)
+		require("lualine").setup({
+			sections = {
+				lualine_c = {
+					{
+						require("noice").api.statusline.mode.get,
+						cond = require("noice").api.statusline.mode.has,
+						color = { fg = require("catppuccin.palettes").get_palette("mocha").red },
+					},
+				},
+			},
 		})
 
 		require("noice").setup({
@@ -20,6 +34,7 @@ return {
 					["cmp.entry.get_documentation"] = true,
 				},
 			},
+
 			-- you can enable a preset for easier configuration
 			presets = {
 				bottom_search = true, -- use a classic bottom cmdline for search
@@ -27,6 +42,21 @@ return {
 				long_message_to_split = true, -- long messages will be sent to a split
 				inc_rename = false, -- enables an input dialog for inc-rename.nvim
 				lsp_doc_border = true, -- add a border to hover docs and signature help
+			},
+
+			-- messages = { enabled = false },
+			-- notify = { enabled = false },
+
+			routes = {
+				{
+					filter = {
+						event = "notify",
+						find = "No information available",
+					},
+					opts = {
+						skip = true,
+					},
+				},
 			},
 		})
 	end,
