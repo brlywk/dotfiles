b20e27d6ed31521ec781af05d27067cd125746cd
Christoph Suessmeier <christoph.suessmeier@exxeta.com>
2024-06-17 14:58:25 +0200

    Added work config

--- .chezmoiscripts/run_once_after_setup-neovim.zsh
+++ .chezmoiscripts/run_once_after_setup-neovim.zsh
@@ -1,10 +1,12 @@
 #!/bin/zsh
 
-# Install some necessary packages
-echo "Installing Neovim NPM package..."
-npm install -g neovim
-
 # Clone neovim config
 if [ ! -d "$HOME/.config/nvim" ]; then
     git clone gh:brlywk/nvim ~/.config/nvim
+else
+    echo "Neovim already set up..."
 fi
+
+# Install some necessary packages
+echo "Installing Neovim NPM package..."
+npm install -g neovim
